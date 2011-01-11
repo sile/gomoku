3d9a751222aa94329c17d907df2c0831ee78927b
sile <phjgt308@gmail.com>
2011-01-12 08:19:31 +0900

    整数出力関数作成

---
+++ dicbuilder/util.lisp
@@ -0,0 +1,13 @@
+(in-package :gomoku)
+
+(declaim (inline write-int))
+
+(defun write-int (int stream &key (width 1) (endian :big))
+  (declare ((member '(:big :little)) endian)
+           ((member '(1 2 4)) width))
+  (flet ((write-impl (pos) 
+           (write-byte (ldb (byte 8 (* pos 8)) int) stream)))
+    (declare (inline write-impl))
+    (case endian
+      (:big    (loop FOR i FROM (1- width) DOWNTO 0 DO (write-impl i)))
+      (:little (loop FOR i FROM 0 BELOW width       DO (write-impl i))))))
